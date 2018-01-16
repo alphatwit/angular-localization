6dff131d49c3c809b30dfd4fdc140d90a4ab89ff
Tanya <tanya.mounitsyna@gmail.com>
2018-01-16 13:26:12 -0500

    Change .success to .then
    Angular 1.6 doesn't support .success https://stackoverflow.com/questions/41169385/http-get-success-is-not-a-function

--- dist/angular-localization.js
+++ dist/angular-localization.js
@@ -127,7 +127,7 @@ angular.module('ngLocalize')
                     url += localeConf.fileExtension;
 
                     $http.get(url)
-                        .success(function (data) {
+                        .then(function (data) {
                             var key,
                                 path = getPath(token);
                             // Merge the contents of the obtained data into the stored bundle.
@@ -153,8 +153,7 @@ angular.module('ngLocalize')
                             if (deferrences[path]) {
                                 deferrences[path].resolve(path);
                             }
-                        })
-                        .error(function (err) {
+                        },function (err) {
                             var path = getPath(token);
 
                             $log.error('[localizationService] Failed to load: ' + url);
